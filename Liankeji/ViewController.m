e4a3c9a4ed7deb2e2044e88e7fe3eec511d41e83
qicaitiankong <zihaoshuhan@163.com>
2016-11-29 09:14:56 +0800

    Initial Commit

---
+++ Liankeji/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Liankeji
+//
+//  Created by 李自豪 on 16/11/29.
+//  Copyright © 2016年 haichuanhuiwulianxinxi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Liankeji/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Liankeji
+//
+//  Created by 李自豪 on 16/11/29.
+//  Copyright © 2016年 haichuanhuiwulianxinxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
